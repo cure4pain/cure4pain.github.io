6caa56a8ac1013e1cb0610d72ff03bae7ee7ec64
cure4pain <44055326+cure4pain@users.noreply.github.com>
2018-10-11 15:35:39 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+
+
